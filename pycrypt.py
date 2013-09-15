28e1aaf0c5548fc281c085c1ba3bcf69186d51ec
Matej Hlavacek <matej.hl@gmail.com>
2013-09-15 12:35:30 +0200

    fixed scoring, using permutations instead of variations

--- pycrypt.py
+++ pycrypt.py
@@ -7,7 +7,7 @@ import operator
 import pyevolve
 from pyevolve import G1DList
 from pyevolve import GSimpleGA
-from pyevolve import Selectors
+from pyevolve import Selectors, Mutators
 from cProfile import run
 
 class Decoder ():
@@ -20,21 +20,18 @@ class Decoder ():
 	for i in ENGLISH_TRIGRAMS.keys():
 		ENGLISH_TRIGRAMS[i] /= 10000.0
 
-	def __init__ (self, ciphered_string, ideal_frequencies = [ENGLISH_FREQUENCY, ENGLISH_BIGRAMS, ENGLISH_TRIGRAMS]):
+	def __init__(self, ciphered_string, ideal_frequencies = [ENGLISH_FREQUENCY, ENGLISH_BIGRAMS, ENGLISH_TRIGRAMS]):
 		self.words = None
 		self.maxWordLen = 10
 		self.minWordLen = 3
-		self.text_frequencies = []
 		self.ideal_frequencies = ideal_frequencies
-		for i in range(1,4):
-			self.text_frequencies.append(self.getFrequencies(ciphered_string, i))
 		self.ciphered_string = ciphered_string
 
-	def loadWords (self, path):
+	def loadWords(self, path):
 		self.words = set([line.strip().upper() for line in open(path)])
 		self.maxWordLen = 10
 
-	def applyKey (self, s, key):
+	def applyKey(self, s, key):
 		f = ""
 		for c in s:
 			if (key.has_key(c)):
@@ -43,7 +40,7 @@ class Decoder ():
 				f += c
 		return f
 
-	def getFrequencies (self, s, length):
+	def getFrequencies(self, s, length):
 		d = {}
 		for i in range(len(s) - 1 - length):
 			if (d.has_key(s[i:i+length])):
@@ -53,47 +50,46 @@ class Decoder ():
 
 		return d
 
-	def applyReversedKey (self, s, key):
+	def applyReversedKey(self, s, key):
 		return self.applyKey(s, dict(zip(key.values(), key.keys())))
 
-	def getReversedKey (self, key):
+	def getReversedKey(self, key):
 		return dict(zip(key.values(), key.keys()))
 
-	def generateRandomKey (self):
+	def generateRandomKey(self):
 		shuf = list(self.alphabet)
 		random.shuffle(shuf)
 		"".join(shuf)
 		return dict(zip(self.alphabet, shuf))
 
-	def getScoreFreq (self, s, f, key):
+	def getScoreFreq(self, s, f, key):
 		d = {}
 		for i, j in key.items():
 			d[j] = f[i]
 		return 1 - sum(map(lambda a: abs(a[0] - a[1]) / 2, zip(d.values(), self.baseFrequency.values())))
 
-	def getScoresGlist (self, glist, log=False):
+	def getScoresGlist(self, glist, log=False):
 		return self.getScores(self.glistToKey(glist), log)
 
-	def getScores (self, key, log=False):
+	def getScores(self, key, log=False):
 		scores = self.getScoreNgrams(key)
 
 		result_score = 0
-		displace_index = 0
-		for letter in d.alphabet:
-			letter_count = 0
-			for key_letter in key.values():
-				if letter == key_letter:
-					letter_count += 1
-			if letter_count > 1:
-				displace_index += letter_count
-		displace_index = 26 - displace_index
-		score_words = self.getScoreWords(key)
-		result_score = scores[0]/40 + scores[1]/2 + scores[2] / 1.4 + displace_index / 26.0
-		result_score += score_words / 4
+		# displace_index = 0
+		# for letter in d.alphabet:
+		# 	letter_count = 0
+		# 	for key_letter in key.values():
+		# 		if letter == key_letter:
+		# 			letter_count += 1
+		# 	if letter_count > 1:
+		# 		displace_index += letter_count
+		# displace_index = 26 - displace_index
+		score_words = 0 #self.getScoreWords(key)
+		result_score = scores[0]/200 + scores[1]/2 + scores[2] / 1.5# + displace_index / 26.0
+		#result_score += score_words / 4
 
 		if log == True:
-			print scores, displace_index, score_words
-			print scores[0]/60, scores[1]/2, scores[2] / 1.5, displace_index / 26.0, score_words / 4
+			print scores[0]/200, scores[1]/2, scores[2] / 1.5,# displace_index / 26.0, score_words / 4
 			print result_score
 
 		return result_score
@@ -104,29 +100,22 @@ class Decoder ():
 			ret.append(str(unichr(i)))
 		return dict(zip(d.alphabet, ret))
 
-	def getScoreNgrams (self, key):
+	def getScoreNgrams(self, key):
 		scores = []
-		for i, text_frequency in enumerate(self.text_frequencies):
-			used_ngrams = {}
+		freqs = [self.getFrequencies(self.ciphered_string, i) for i in range(1, 4)]
+		for i, f in enumerate(freqs):
 			translated_freq = {}
 			scores.append(0)
 
-			for ngram, ngram_freq in text_frequency.items():
-				translated_ngram = self.applyKey(ngram, key)
-				if self.ideal_frequencies[i].has_key(translated_ngram):
-					score = self.ideal_frequencies[i][translated_ngram] * ngram_freq * len(self.ciphered_string)
-					if not used_ngrams.has_key(translated_ngram):
-						used_ngrams[translated_ngram] = score
-					else:
-						used_ngrams[translated_ngram] = min(score, used_ngrams[translated_ngram])
+			for ngram, ngram_freq in f.items():
+				translated_freq[self.applyKey(ngram, key)] = ngram_freq
 
-			for j in used_ngrams.values():
-				scores[i] += j
+			for ngram in list(set(self.ideal_frequencies[i].keys()) & set(translated_freq.keys())):
+				scores[i] += self.ideal_frequencies[i][ngram]*translated_freq[ngram]*len(self.ciphered_string)
 
 		return scores
 
-
-	def getScoreWords (self, key):
+	def getScoreWords(self, key):
 		if (self.maxWordLen == 0):
 			return 0
 
@@ -139,11 +128,11 @@ class Decoder ():
 
 		pts /= len(s)
 		return (pts ** 2) * 0.8
-
-lipsum = "The White-bellied Sea Eagle is a large diurnal bird of prey in the family Accipitridae. A distinctive bird, adults have a white head, breast, under-wing coverts and tail."
+		
+lipsum = "The White-bellied Sea Eagle is a large diurnal bird of prey in the family Accipitridae. A distinctive bird, adults have a white head, breast, under-wing coverts and tail. The upper parts are grey and the black under-wing flight feathers contrast with the white coverts."
 #The upper parts are grey and the black under-wing flight feathers contrast with the white coverts. Like many raptors, the female is slightly larger than the male, and can measure up to 90 cm (36 in) long with a wingspan of up to 2.2 m (7 ft), and weigh 4.5 kg (10 lb). The call is a loud goose-like honking. Resident from India and Sri Lanka through southeast Asia to Australia on coasts and major waterways, the White-bellied Sea Eagle breeds and hunts near water, and fish form around half of its diet. Opportunistic, it consumes carrion and a wide variety of animals. Although rated of Least Concern globally, it has declined in parts of southeast Asia such as Thailand, and southeastern Australia. Human disturbance to its habitat is the main threat, both from direct human activity near nests which impacts on breeding success, and from removal of suitable trees for nesting. The White-bellied Sea Eagle is revered by indigenous people in many parts of Australia, and is the subject of various folk tales throughout its range."
 
-lipsum = "hereuponlegrandarosewithagraveandstatelyairandbroughtmethebeetlefromaglasscaseinwhichitwasencloseditwasabeautifulscarabaeusandatthattimeunknowntonaturalistsofcourseagreatprizeinascientificpointofviewthereweretworoundblackspotsnearoneextremityofthebackandalongoneneartheotherthescaleswereexceedinglyhardandglossywithalltheappearanceofburnishedgoldtheweightoftheinsectwasveryremarkableandtakingallthingsintoconsiderationicouldhardlyblamejupiterforhisopinionrespectingit"
+# lipsum = "hereuponlegrandarosewithagraveandstatelyairandbroughtmethebeetlefromaglasscaseinwhichitwasencloseditwasabeautifulscarabaeusandatthattimeunknowntonaturalistsofcourseagreatprizeinascientificpointofviewthereweretworoundblackspotsnearoneextremityofthebackandalongoneneartheotherthescaleswereexceedinglyhardandglossywithalltheappearanceofburnishedgoldtheweightoftheinsectwasveryremarkableandtakingallthingsintoconsiderationicouldhardlyblamejupiterforhisopinionrespectingit"
 #random.seed(1)
 
 lipsum = lipsum.upper()
@@ -154,32 +143,46 @@ d.getScores(dict(zip(d.alphabet, d.alphabet)), log=True)
 print lipsum[:100]
 
 def callback(ga):
-	if not ga.getCurrentGeneration() % 30:
+	# print sorted(ga.bestIndividual())
+	if not ga.getCurrentGeneration() % 10:
 		result_genome = ga.bestIndividual()
 		d.getScoresGlist(result_genome, log=True)
 		b = d.glistToKey(result_genome)
 		print d.applyKey(d.ciphered_string, b)
 	return False
 
+def G1DListPermInitializator(genome, **args):
+	genome.clearList()
+	a = range(genome.getListSize())
+	random.shuffle(a)
+	for i in a:
+		genome.append(i + 65)
+
+
 genome = G1DList.G1DList(26)
 genome.setParams(rangemin=65, rangemax=90)
+genome.initializator.set(G1DListPermInitializator)
 genome.evaluator.set(d.getScoresGlist)
 ga = GSimpleGA.GSimpleGA(genome)
-ga.selector.set(Selectors.GTournamentSelector)
+# ga.selector.set(Selectors.GTournamentSelector)
 
-#for i in range(10):
-#ga.setCrossoverRate(0)
-#ga.setMutationRate(0.05)
+# for i in range(10):
+ga.setCrossoverRate(0)
+ga.setMutationRate(0.5)
 ga.stepCallback.set(callback)
 
-#ga.setPopulationSize(10000)
-#ga.setGenerations(1)
-#ga.evolve(freq_stats=10)
+# ga.setPopulationSize(1000)
+# ga.setGenerations(1)
+# ga.evolve(freq_stats=10)
 #
-#ga.setPopulationSize(100)
-ga.setGenerations(100)
+ga.setPopulationSize(40)
+ga.setGenerations(200)
 
-run("ga.evolve(freq_stats=30)")
+sqlite_adapter = pyevolve.DBAdapters.DBSQLite(identify="ex1")
+ga.setDBAdapter(sqlite_adapter)
+
+# run("ga.evolve(freq_stats=30)")
+ga.evolve(freq_stats=10)
 
 result_genome = ga.bestIndividual()
 print d.getScoresGlist(result_genome, log=True)
