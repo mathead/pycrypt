62be17b2eec7ac7ee49d097b411df712ec55c4a3
Matej Hlavacek <matej.hl@gmail.com>
2013-10-17 13:03:16 +0200

    vigenere test

--- pycrypt/utils.py
+++ pycrypt/utils.py
@@ -2,8 +2,11 @@ import string
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 alphabet = string.uppercase
+alphabetWithCh = list(string.uppercase)
+alphabetWithCh.insert(8, 'CH')
 
 def split(string):
 	return re.split('\W+', string)
@@ -15,7 +18,7 @@ def line_split(string):
 		return string
 
 def array_concat(raw_arrays):
-	"""concats 3d numpy arrays to one big one, lines don't have to be the same size"""
+	"""concats 2d numpy arrays to one big one, lines don't have to be the same size"""
 	arrays = []
 	for i in raw_arrays:
 		h = np.hstack(i)
@@ -39,4 +42,33 @@ def array_concat(raw_arrays):
 def plot_array(arr):
 	plt.imshow(arr, cmap=plt.cm.binary, interpolation='nearest')
 	plt.xticks([]), plt.yticks([])
+	plt.show()
+
+def get_frequency(string, freq_alphabet=alphabet, ratio=False):
+	"""Count frequency of given alphabet, if None, count every char. Set ratio True to divide by length"""
+	if (freq_alphabet):
+		string = string.upper()
+		f = OrderedDict.fromkeys(freq_alphabet, 0.0)
+		for c in string:
+			if (f.has_key(c)):
+				f[c] += 1
+	else:
+		f = {}
+		for c in string:
+			if (f.has_key(c)):
+				f[c] += 1
+			else:
+				f[c] = 1.0
+
+	if (ratio):
+		suma = sum(f.values())
+		for i in f:
+			f[i] /= suma
+
+	return f
+
+def plot_dict(d):
+	val = d
+	plt.bar(range(len(val)), np.array(val.values()))
+	plt.xticks(np.arange(len(val))+0.4, d.keys())
 	plt.show()
