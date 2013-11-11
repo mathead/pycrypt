6684e2ce32c9579376855fd69f0e4aa92187ed88
PrehistoricTeam <matej.hl@gmail.com>
2013-11-11 23:00:42 +0100

    added frequencies to init

--- pycrypt/scorers/__init__.py
+++ pycrypt/scorers/__init__.py
@@ -2,3 +2,5 @@ from scorer import *
 from languagescorer import *
 from czechscorer import *
 from englishscorer import *
+import czechfrequencies as CzechFrequencies
+import englishfrequencies as EnglishFrequencies
