85ed16f9262d7f8681ba9b906d7ac434fbe2c09d
Matej Hlavacek <matej.hl@gmail.com>
2013-11-26 12:55:10 +0100

    NumberKeyGenerator incomplete

--- pycrypt/keygenerators/keygenerator.py
+++ pycrypt/keygenerators/keygenerator.py
@@ -1,12 +1,22 @@
 class KeyGenerator():
-	"""Abstract class for generating and scoring keys for specific Translator"""
+	"""Abstract class for generating keys for specific Translator"""
 
-	def __init__(self, translator=None, scorer=None):
-		pass
+	def __init__(self, translator=None):
+		self.translator = translator
 
 	def getRandomKey(self):
 		"""Random key i.e. for starting genetic population"""
 		raise NotImplementedError()
 
 	def getAllKeys(self):
-		"""Get all possible keys, list generator nejlepe""" ###############TODO
+		"""Get all possible keys, python generator preferably"""
+		raise NotImplementedError()
+
+	def mutateKey(self):
+		"""For genetics - get similar key"""
+		raise NotImplementedError()
+
+	def translate(self, key):
+		if (self.translator == None):
+			raise AttributeError("No translator set")
+		return self.translator.translate()
