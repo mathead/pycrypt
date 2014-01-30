72ed23450872bddd9d0bc249379800e4516aafea
PrehistoricTeam <matej.hl@gmail.com>
2014-01-30 22:25:32 +0100

    restructured as python package

---
+++ README.txt
@@ -0,0 +1,1 @@
+You can check out the docs at http://pycrypt.readthedocs.org/en/latest/
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    name="pycrypt",
-    version="0.1",
+    name='pycrypt',
+    version='0.1',
+    author='Matej Hlavacek',
+    author_email='hlavacek.matej@gmail.com',
+    packages=['pycrypt'],
+    license='LICENSE.txt',
+    description='Pycrypt is a python suite for solving ciphers at (mostly Czech) cryptography games.',
+    long_description=open('README.txt').read(),
     install_requires=[
         "numpy",
         "pyplot",
