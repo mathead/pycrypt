e2f770dda0eea22bcddbdf402252873f4f78ce33
PrehistoricTeam <matej.hl@gmail.com>
2014-01-30 21:48:06 +0100

    commit for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -243,8 +243,8 @@ texinfo_documents = [
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
 
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
