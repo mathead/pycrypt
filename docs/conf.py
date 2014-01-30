f348e33fbebc4583e746c2a1fb82c9ed891e00d6
PrehistoricTeam <matej.hl@gmail.com>
2014-01-30 22:54:10 +0100

    mock fix

--- docs/conf.py
+++ docs/conf.py
@@ -10,14 +10,14 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+
+
+import sys, os
 import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
-
-
-import sys, os
+	sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
