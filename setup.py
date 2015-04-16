c270172c3da8e3ce626be9b0362a50f403d5f629
Matej Hlavacek <matej.hl@gmail.com>
2015-04-16 21:38:05 +0200

    subpackage structure fix

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
 setup(
     name='pycrypt',
     version='0.2',
     author='Matej Hlavacek',
     author_email='hlavacek.matej@gmail.com',
-    packages=['pycrypt'],
+    packages=find_packages(),
     license='LICENSE.txt',
     description='Pycrypt is a python suite for solving ciphers at (mostly Czech) cryptography games.',
     long_description=open('README.txt').read(),
